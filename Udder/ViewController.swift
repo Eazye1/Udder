ad298c1516cadbccec0a75ddb7a4255b483dbe25
Ezra Laboz <eddielaboz@yahoo.com>
2022-07-27 11:51:37 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ezra Laboz on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
